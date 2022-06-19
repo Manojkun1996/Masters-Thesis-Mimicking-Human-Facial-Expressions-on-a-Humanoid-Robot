0fe971dbe7d5f57ce80b3b58278e177b0daa549d
Manojkun1996 <88244126+Manojkun1996@users.noreply.github.com>
2022-06-19 19:58:55 +0200

    Update Text_to_Speech.py

--- Text_to_Speech.py
+++ Text_to_Speech.py
@@ -2,11 +2,6 @@ from hr_little_api.robot import Robot
 from hr_little_api.functional import say, go_crazy, poke_tounge, right_arm_point, walk_forward
 from hr_little_api.robot import Animation
 from hr_little_api.functional import say, go_crazy, poke_tounge, right_arm_point, walk_forward
-
-
-
-
-
 robot = Robot()
 robot.connect()
 
@@ -18,11 +13,7 @@ with sr.Microphone() as source:
     try:
         text1 = r.recognize_google(audio)
         print("You said : {}".format(text1))
-        
         robot.say ("{}".format(text1))
-
-
-
     except:
         x = print("Sorry could not recognize what you said")
 
